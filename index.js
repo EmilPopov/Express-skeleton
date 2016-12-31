96157213574dd7dd744322e907a9ad9d0122b373
Emil Popov <emilpopov13@yahoo.com>
2016-12-31 08:28:30 +0200

    create public and server folders

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var hbs = require('express-handlebars');
+var path = require('path');
 
 var app = express();
+// set static files
+app.use(express.static('public'));
 
+app.set('views', path.join(__dirname, './server/views/partials'));
+app.engine('handlebars', hbs({
+  partialsDir   : __dirname +'/server/views/partials',
+  defaultLayout : __dirname +'/server/views/layouts/default',
+  // extname       : '.hbs',
+  layoutsDir    : __dirname +'/server/views/layouts'
+}));
+app.set('view engine', 'handlebars');
+
+// set environment
+var env = process.env.NODE_ENV || 'development';
+var port = process.env.port || 1234;
+
+// set db
 mongoose.Promise = global.Promise;
-var dbPath = 'mongodb://localhost:27017/express-skeleton';
-var connection = dbPath;
-
+var connection = 'mongodb://localhost:27017/express-skeleton';
 mongoose.connect(connection)
     .then(function() {
         console.log('MongoDB up and runnings !!!');
     })
     .catch(console.log);
 
+
 app.get('/', function(req, res){
-  res.send('Welcome to my app');
+  res.render('index', {title:'Welcome to my app'});
 });
 
-app.listen(1234);
+app.listen(port);
